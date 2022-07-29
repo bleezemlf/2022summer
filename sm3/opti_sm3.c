a36f9445d6946a0fdde41c7508e32b5ebb929939
bleezemlf <bleezemlf@gmail.com>
2022-07-29 14:05:35 +0800

    finish sm3

--- sm3/opti_sm3.c
+++ sm3/opti_sm3.c
@@ -7,9 +7,14 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
+#include <xmmintrin.h>
 #define BUFFER_SIZE 1024
 #define SM3_MSG_BLOCK_BYTESIZE 64
 #define SM3_VECTOR_BLOCK_BYTESIZE 32
+
+static uint32_t Tj[] = {
+    0x79cc4519U, 0xf3988a32U, 0xe7311465U, 0xce6228cbU, 0x9cc45197U, 0x3988a32fU, 0x7311465eU, 0xe6228cbcU, 0xcc451979U, 0x988a32f3U, 0x311465e7U, 0x6228cbceU, 0xc451979cU, 0x88a32f39U, 0x11465e73U, 0x228cbce6U, 0x9d8a7a87U, 0x3b14f50fU, 0x7629ea1eU, 0xec53d43cU, 0xd8a7a879U, 0xb14f50f3U, 0x629ea1e7U, 0xc53d43ceU, 0x8a7a879dU, 0x14f50f3bU, 0x29ea1e76U, 0x53d43cecU, 0xa7a879d8U, 0x4f50f3b1U, 0x9ea1e762U, 0x3d43cec5U, 0x7a879d8aU, 0xf50f3b14U, 0xea1e7629U, 0xd43cec53U, 0xa879d8a7U, 0x50f3b14fU, 0xa1e7629eU, 0x43cec53dU, 0x879d8a7aU, 0x0f3b14f5U, 0x1e7629eaU, 0x3cec53d4U, 0x79d8a7a8U, 0xf3b14f50U, 0xe7629ea1U, 0xcec53d43U, 0x9d8a7a87U, 0x3b14f50fU, 0x7629ea1eU, 0xec53d43cU, 0xd8a7a879U, 0xb14f50f3U, 0x629ea1e7U, 0xc53d43ceU, 0x8a7a879dU, 0x14f50f3bU, 0x29ea1e76U, 0x53d43cecU, 0xa7a879d8U, 0x4f50f3b1U, 0x9ea1e762U, 0x3d43cec5U
+};
 
 #define debugPrintA_H printf("%02d: %08x %08x %08x %08x %08x %08x %08x %08x \n", j, A, B, C, D, E, F, G, H);
 
@@ -24,11 +29,12 @@
     | (0x0000ff00 & x >> 8)                   \
     | (0x000000ff & x >> 24)
 
-static uint32_t IV[] = { 0x7380166f, 0x4914b2b9, 0x172442d7, 0xda8a0600, 0xa96f30bc, 0x163138aa, 0xe38dee4d, 0xb0fb0e4e };
+static uint32_t IV[]
+    = { 0x7380166f, 0x4914b2b9, 0x172442d7, 0xda8a0600, 0xa96f30bc, 0x163138aa, 0xe38dee4d, 0xb0fb0e4e };
 
 typedef struct sm3ctx {
-    uint32_t v[SM3_VECTOR_BLOCK_BYTESIZE / sizeof(uint32_t)];
-    uint8_t msg[SM3_MSG_BLOCK_BYTESIZE];
+    uint32_t v[8];
+    uint8_t msg[8][64];
     size_t num;
     size_t nblocks;
 } Sm3Ctx;
@@ -50,93 +56,119 @@ static inline void debugPrint(uint32_t* msg, size_t len)
     printf("\n\n");
 }
 
-static inline uint32_t rol(uint32_t data, uint32_t len)
-{
-    return data << len | data >> (sizeof(data) * 8 - len);
-}
+#define rol(data, len) (((data) << (len)) | ((data) >> (32 - (len))))
+#define P0(X) (X ^ rol(X, 9) ^ rol(X, 17))
+#define P1(X) (X ^ rol(X, 15) ^ rol(X, 23))
+#define FF0(X, Y, Z) (X ^ Y ^ Z)
+#define FF1(X, Y, Z) ((X & Y) | (X & Z) | (Y & Z))
+#define GG0(X, Y, Z) (X ^ Y ^ Z)
+#define GG1(X, Y, Z) ((X & Y) | (~X & Z))
 
-static inline uint32_t P0(uint32_t X)
+#define loadw(x) _mm256_set_epi32(((uint32_t*)(msg[7]))[x], ((uint32_t*)(msg[6]))[x], ((uint32_t*)(msg[5]))[x], ((uint32_t*)(msg[4]))[x], ((uint32_t*)(msg[3]))[x], ((uint32_t*)(msg[2]))[x], ((uint32_t*)(msg[1]))[x], ((uint32_t*)(msg[0]))[x])
+#define rol_256_epi32(x, i) _mm256_xor_si256(_mm256_slli_epi32((x), (i)), _mm256_srli_epi32((x), 32 - (i)))
+#define P1_256_epi32(x) _mm256_xor_si256((x), (_mm256_xor_si256(rol_256_epi32((x), 15), rol_256_epi32((x), 23))))
+
+#define R(A, B, C, D, E, F, G, H, x)                             \
+    SS1 = rol((rol(A, 12) + E + Tj[j]), 7);                      \
+    SS2 = SS1 ^ rol(A, 12);                                      \
+    TT1 = FF##x(A, B, C) + D + SS2 + (((uint32_t*)(W1 + j)))[i]; \
+    TT2 = GG##x(E, F, G) + H + SS1 + (((uint32_t*)(W + j)))[i];  \
+    B = rol(B, 9);                                               \
+    H = TT1;                                                     \
+    F = rol(F, 19);                                              \
+    D = P0(TT2);                                                 \
+    j++
+
+#define R8(A, B, C, D, E, F, G, H, x) \
+    R(A, B, C, D, E, F, G, H, x);     \
+    R(H, A, B, C, D, E, F, G, x);     \
+    R(G, H, A, B, C, D, E, F, x);     \
+    R(F, G, H, A, B, C, D, E, x);     \
+    R(E, F, G, H, A, B, C, D, x);     \
+    R(D, E, F, G, H, A, B, C, x);     \
+    R(C, D, E, F, G, H, A, B, x);     \
+    R(B, C, D, E, F, G, H, A, x)
+
+static void CF(uint32_t v[32], const uint8_t msg[8][64], int round)
 {
-    return X ^ rol(X, 9) ^ rol(X, 17);
-}
-static inline uint32_t P1(uint32_t X)
-{
-    return X ^ rol(X, 15) ^ rol(X, 23);
-}
-static inline uint32_t FF0(uint32_t X, uint32_t Y, uint32_t Z)
-{
-    return X ^ Y ^ Z;
-}
-static inline uint32_t FF1(uint32_t X, uint32_t Y, uint32_t Z)
-{
-    return (X & Y) | (X & Z) | (Y & Z);
-}
-static inline uint32_t GG0(uint32_t X, uint32_t Y, uint32_t Z)
-{
-    return X ^ Y ^ Z;
-}
-static inline uint32_t GG1(uint32_t X, uint32_t Y, uint32_t Z)
-{
-    return (X & Y) | (~X & Z);
-}
-static void CF(uint32_t v[SM3_VECTOR_BLOCK_BYTESIZE / sizeof(uint32_t)], const uint8_t msg[SM3_MSG_BLOCK_BYTESIZE])
-{
-    uint32_t A = v[0];
-    uint32_t B = v[1];
-    uint32_t C = v[2];
-    uint32_t D = v[3];
-    uint32_t E = v[4];
-    uint32_t F = v[5];
-    uint32_t G = v[6];
-    uint32_t H = v[7];
-    uint32_t W[68], W1[64];
-    const uint32_t* pblock = (const uint32_t*)(msg);
-    for (int j = 0; j < 16; j++) {
-        W[j] = byteSwap_32(pblock[j]);
+    uint32_t A, B, C, D, E, F, G, H;
+    __m256i W[68], W1[64];
+    __m256i temp;
+    __m256i shuffle_v = _mm256_set_epi8(12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3, 12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3);
+    for (int i = 0; i < 16; i++) {
+        W[i] = loadw(i);
+        W[i] = _mm256_shuffle_epi8(W[i], shuffle_v);
     }
-    for (int j = 16; j < 68; j++) {
-        W[j] = P1(W[j - 16] ^ W[j - 9] ^ rol(W[j - 3], 15)) ^ rol(W[j - 13], 7) ^ W[j - 6];
+    for (int i = 16; i < 68; i++) {
+        temp = _mm256_xor_si256(W[i - 16], W[i - 9]);
+        temp = _mm256_xor_si256(temp, rol_256_epi32(W[i - 3], 15));
+        temp = P1_256_epi32(temp);
+        temp = _mm256_xor_si256(temp, rol_256_epi32(W[i - 13], 7));
+        W[i] = _mm256_xor_si256(temp, W[i - 6]);
     }
-    for (int j = 0; j < 64; j++) {
-        W1[j] = W[j] ^ W[j + 4];
+    for (int i = 0; i < 64; i++) {
+        W1[i] = _mm256_xor_si256(W[i], W[i + 4]);
     }
-    for (int j = 0; j < 16; j++) {
-        uint32_t SS1 = rol(rol(A, 12) + E + rol(0x79cc4519, j), 7);
-        uint32_t SS2 = SS1 ^ rol(A, 12);
-        uint32_t TT1 = FF0(A, B, C) + D + SS2 + W1[j];
-        uint32_t TT2 = GG0(E, F, G) + H + SS1 + W[j];
-        D = C;
-        C = rol(B, 9);
-        B = A;
-        A = TT1;
-        H = G;
-        G = rol(F, 19);
-        F = E;
-        E = P0(TT2);
+    for (int i = 0; i < round; i++) {
+        A = v[0];
+        B = v[1];
+        C = v[2];
+        D = v[3];
+        E = v[4];
+        F = v[5];
+        G = v[6];
+        H = v[7];
+
+        uint32_t SS1, SS2, TT1, TT2;
+        int j = 0;
+
+        R8(A, B, C, D, E, F, G, H, 0);
+        R8(A, B, C, D, E, F, G, H, 0);
+        R8(A, B, C, D, E, F, G, H, 1);
+        R8(A, B, C, D, E, F, G, H, 1);
+        R8(A, B, C, D, E, F, G, H, 1);
+        R8(A, B, C, D, E, F, G, H, 1);
+        R8(A, B, C, D, E, F, G, H, 1);
+        R8(A, B, C, D, E, F, G, H, 1);
+
+        /* for (j = 0; j < 16; j++) { */
+        /*     SS1 = rol(rol(A, 12) + E + Tj[j], 7); */
+        /*     SS2 = SS1 ^ rol(A, 12); */
+        /*     TT1 = FF0(A, B, C) + D + SS2 + (((uint32_t*)(W1 + j)))[i]; */
+        /*     TT2 = GG0(E, F, G) + H + SS1 + (((uint32_t*)(W + j)))[i]; */
+        /*     D = C; */
+        /*     C = rol(B, 9); */
+        /*     B = A; */
+        /*     A = TT1; */
+        /*     H = G; */
+        /*     G = rol(F, 19); */
+        /*     F = E; */
+        /*     E = P0(TT2); */
+        /* } */
+        /* for (j = 16; j < 64; j++) { */
+        /*     SS1 = rol(rol(A, 12) + E + Tj[j], 7); */
+        /*     SS2 = SS1 ^ rol(A, 12); */
+        /*     TT1 = FF1(A, B, C) + D + SS2 + (((uint32_t*)(W1 + j)))[i]; */
+        /*     TT2 = GG1(E, F, G) + H + SS1 + (((uint32_t*)(W + j)))[i]; */
+        /*     D = C; */
+        /*     C = rol(B, 9); */
+        /*     B = A; */
+        /*     A = TT1; */
+        /*     H = G; */
+        /*     G = rol(F, 19); */
+        /*     F = E; */
+        /*     E = P0(TT2); */
+        /*     /1* debugPrintA_H; *1/ */
+        /* } */
+        v[0] ^= A;
+        v[1] ^= B;
+        v[2] ^= C;
+        v[3] ^= D;
+        v[4] ^= E;
+        v[5] ^= F;
+        v[6] ^= G;
+        v[7] ^= H;
     }
-    for (int j = 16; j < 64; j++) {
-        uint32_t SS1 = rol(rol(A, 12) + E + rol(0x7a879d8a, j), 7);
-        uint32_t SS2 = SS1 ^ rol(A, 12);
-        uint32_t TT1 = FF1(A, B, C) + D + SS2 + W1[j];
-        uint32_t TT2 = GG1(E, F, G) + H + SS1 + W[j];
-        D = C;
-        C = rol(B, 9);
-        B = A;
-        A = TT1;
-        H = G;
-        G = rol(F, 19);
-        F = E;
-        E = P0(TT2);
-        /* debugPrintA_H; */
-    }
-    v[0] ^= A;
-    v[1] ^= B;
-    v[2] ^= C;
-    v[3] ^= D;
-    v[4] ^= E;
-    v[5] ^= F;
-    v[6] ^= G;
-    v[7] ^= H;
 }
 
 static void sm3Init(Sm3Ctx* ctx)
@@ -144,60 +176,56 @@ static void sm3Init(Sm3Ctx* ctx)
     memcpy(ctx->v, IV, 32);
     ctx->num = 0;
     ctx->nblocks = 0;
-    memset(ctx->msg, 0, 64);
+    memset(ctx->msg, 0, 512);
 }
 
 static void sm3Update(Sm3Ctx* ctx, const uint8_t* msg, size_t msg_len)
 {
-    /* for (int i = 0; i < 64; i++) { */
-    /*     printf("%02x", ctx->msg[i]); */
-    /* } */
     if (ctx->num) {
-        size_t left = SM3_MSG_BLOCK_BYTESIZE - ctx->num;
+        size_t left = 512 - ctx->num;
         if (msg_len < left) {
             memcpy(ctx->msg + ctx->num, msg, msg_len);
             ctx->num += msg_len;
             return;
         } else {
             memcpy(ctx->msg + ctx->num, msg, left);
-            CF(ctx->v, ctx->msg);
-            ctx->nblocks++;
+            CF(ctx->v, ctx->msg, 8);
+            ctx->nblocks += 8;
             msg += left;
             msg_len -= left;
         }
     }
     while (msg_len >= SM3_MSG_BLOCK_BYTESIZE) {
-        CF(ctx->v, msg);
-        ctx->nblocks++;
-        msg += SM3_MSG_BLOCK_BYTESIZE;
-        msg_len -= SM3_MSG_BLOCK_BYTESIZE;
+        CF(ctx->v, msg, 8);
+        ctx->nblocks += 8;
+        msg += 512;
+        msg_len -= 512;
     }
     ctx->num = msg_len;
     if (msg_len) {
         memcpy(ctx->msg, msg, msg_len);
     }
-    /* for (int i = 0; i < 64; i++) { */
-    /*     printf("%02x", ctx->msg[i]); */
-    /* } */
-    /* printf("\n"); */
 }
 
 static void sm3Final(Sm3Ctx* ctx, uint8_t* v)
 {
     size_t i;
+    int round = ctx->num / 64;
     uint32_t* t_v = (uint32_t*)(v);
-    uint64_t* count = (uint64_t*)(ctx->msg + SM3_MSG_BLOCK_BYTESIZE - 8);
-    ctx->msg[ctx->num] = 0x80;
+    uint64_t* count = (uint64_t*)((uint8_t*)(ctx->msg) + SM3_MSG_BLOCK_BYTESIZE - 8);
+    *((uint8_t*)(ctx->msg) + (ctx->num)) = 0x80;
     if (ctx->num + 9 <= SM3_MSG_BLOCK_BYTESIZE) {
+        round++;
         memset(ctx->msg + ctx->num + 1, 0, SM3_MSG_BLOCK_BYTESIZE - ctx->num - 9);
     } else {
+        round += 2;
         memset(ctx->msg + ctx->num + 1, 0, SM3_MSG_BLOCK_BYTESIZE - ctx->num - 1);
-        CF(ctx->v, ctx->msg);
+        CF(ctx->v, ctx->msg, ctx->num / 8);
         memset(ctx->msg, 0, SM3_MSG_BLOCK_BYTESIZE - 8);
     }
     count[0] = (uint64_t)(ctx->nblocks) * 512 + (ctx->num << 3);
     count[0] = byteSwap_64(count[0]);
-    CF(ctx->v, ctx->msg);
+    CF(ctx->v, ctx->msg, round);
     for (i = 0; i < sizeof(ctx->v) / sizeof(ctx->v[0]); i++) {
         t_v[i] = byteSwap_32(ctx->v[i]);
     }
@@ -231,7 +259,6 @@ void optiSm3FromFile(uint8_t* dgst, const char* file_name)
     }
     rewind(f1);
     char buffer[BUFFER_SIZE]; //存储明文的buffer
-    /* printf("\n%lu\n", file_length); */
     start = clock();
     int i;
     for (i = 0; i < file_length / BUFFER_SIZE; i++) {
@@ -259,19 +286,3 @@ void testOptiSm3()
     for (int i = 0; i < 32; i++)
         printf("%02x", dgst2[i]);
 }
-/* typedef struct SimdSm3ctx { */
-/*     uint8_t msg[8][64]; //消息分块，使用256bit的simd指令集，超出的部分放在该数组中; */
-/* }; */
-
-/* void testSIMD() */
-/* { */
-/*     __m256i a = _mm256_set_epi64x(1, 2, 3, 4); */
-/*     __m256i b = _mm256_set_epi64x(5, 6, 7, 8); */
-/*     __m256i sum = _mm256_add_epi64(a, b); */
-/*     int64_t ra, rb, rc, rd; */
-/*     ra = sum[0]; */
-/*     rb = sum[1]; */
-/*     rc = sum[2]; */
-/*     rd = sum[3]; */
-/*     printf("%ld,%ld,%ld,%ld", ra, rb, rc, rd); */
-/* } */
